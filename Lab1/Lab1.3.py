42571c9d4fdfa7fa830268a6a17154507976a820
DaniilMut <149338700+DaniilMut@users.noreply.github.com>
2023-10-30 02:27:04 +0300

    Add files via upload

---
+++ Lab1/Lab1.3.py
@@ -0,0 +1,10 @@
+list_players = ["Маша", "Петя", "Саша", "Оля", "Кирилл", "Коля"]
+
+# индекс середины
+middle_index = len(list_players) // 2  # 2
+
+left_team = list_players[:middle_index]  # list_players[:2]
+right_team = list_players[middle_index:]  # list_players[2:]
+
+print(left_team)
+print(right_team)
